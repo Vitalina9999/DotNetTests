dcca3c79a6bb22dd5fe1f2a2df6dff1d81a7a8b5
viboiko <vitalina.boiko@dataart.com>
2025-01-06 20:04:05 +0100

    update gitignore

--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be4de71a1b72a2b503baabae15d7830424c1a009")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
