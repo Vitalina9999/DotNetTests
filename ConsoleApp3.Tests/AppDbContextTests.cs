b8e039dc99e38fca7ae52a2e665fa9c346589b5b
viboiko <vitalina.boiko@dataart.com>
2025-02-03 13:41:01 +0100

    qqq

--- ConsoleApp3.Tests/AppDbContextTests.cs
+++ ConsoleApp3.Tests/AppDbContextTests.cs
@@ -10,11 +10,18 @@ namespace ConsoleApp3.Tests
         [Test]
         public void CanAddAndRetrieveUser()
         {
+            Dictionary<string, int> ages = new Dictionary<string, int>();
+            ages.Add("bbb", 20);
+            ages.Add("aaa", 32);
+            foreach (var item in ages)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
             // Arrange
             var user = new Users { Name = "Iaiaia" };
             using (var context = new AppDbContext())
             {
-               
                 context.Users.Add(user);
                 context.SaveChanges();
             }
@@ -26,8 +33,44 @@ namespace ConsoleApp3.Tests
 
                 // Assert
                 Assert.That(retrievedUser, Is.Not.Null);
-                Assert.That(retrievedUser.Name, Is.EqualTo(user.Name));
+                Assert.That(retrievedUser?.Name, Is.EqualTo(user.Name));
             }
+        }
+
+        [Test]
+        public async Task GetDataAsync()
+        {
+            string data = await FetchDataAsync(1);
+            Console.WriteLine(data);
+        }
+
+        async Task<string> FetchDataAsync(int userId)
+        {
+            await using var context = new AppDbContext();
+            var retrievedUser = await context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            return retrievedUser.Name;
+        }
+        
+        [Test]
+        public async Task RunWhenAnyTask()
+        {
+           await RunWhenAnyTaskCompletes();
+        }
+        
+        async Task RunWhenAnyTaskCompletes()
+        {
+            Task.Delay(2000);
+            var task1 = FetchDataAsync(1);
+            Task.Delay(1000);
+            var task2 = FetchDataAsync(2);
+            Task.Delay(1000);
+            var task3 = FetchDataAsync(3);
+            List<Task> tasks = new List<Task>() { task1, task2, task3 };
+            while (tasks.Count > 0)
+            {
+                Task completedTask = await Task.WhenAny(tasks);
+                tasks = tasks.Where(t => t != completedTask).ToList();
+                Console.WriteLine(await (Task<string>)completedTask);            }
         }
     }
 }
---
+++ ConsoleApp3.Tests/Read.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp3.Tests;
+
+public class Read
+{
+    public const int Age = 20;
+    private static string _name = "aaa";
+    public readonly string SurName = "bbb";
+    
+    public Read(string SurName)
+    {
+        SurName = "aaa";
+        Console.WriteLine(SurName);
+    }
+  
+}
